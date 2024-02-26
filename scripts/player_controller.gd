353537e2966d7136ba48b2bf268b22ca5d172c48
ElecarnoYT <benrobmc.mc@gmail.com>
2024-02-26 19:20:29 +0000

    jump sfx and stamina bar

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -26,8 +26,8 @@ var gravity = 19.6
 @onready var head: Node3D = get_node("head")
 @onready var cam: Camera3D = get_node("head/cam")
 @onready var coyote_timer: Timer = get_node("coyote_time")
+@onready var sprint_cooldown_timer: Timer = get_node("sprint_cooldown")
 @onready var col: CollisionShape3D = get_node("col")
-
 @onready var sfx: player_sfx = get_node("head/sfx")
 
 func _ready():
@@ -47,9 +47,26 @@ func _physics_process(delta):
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
+	# movement direction
+	var input_dir
+	if !player_stats.hp <= 0:
+		input_dir = Input.get_vector("mov_left", "mov_right", "mov_forward", "mov_backward")
+	else:
+		input_dir = Vector2(0, 0)
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	# gravity
 	if !is_on_floor():
 		velocity.y -= gravity * delta
+		
+	# handle death
+	if player_stats.hp <= 0:
+		col.shape.height = 0.1
+		cam.rotation.z = lerp(cam.rotation.z, deg_to_rad(90), delta * 2.0)
+		velocity.x = lerp(velocity.x, direction.x, delta * 3.0)
+		velocity.z = lerp(velocity.z, direction.z, delta * 3.0)
+		move_and_slide()
+		return
 		
 	# coyote_time
 	if is_on_floor() and !grounded:
@@ -58,8 +75,9 @@ func _physics_process(delta):
 		coyote_timer.start()
 
 	# handle jump.
-	if Input.is_action_just_pressed("mov_jump") and is_on_floor():
+	if Input.is_action_just_pressed("mov_jump") and is_on_floor() and player_stats.stamina >= (player_stats.STAMINA_DRAIN * 1.5):
 		velocity.y = JUMP_VELOCITY
+		player_stats.stamina -= player_stats.STAMINA_DRAIN * 1.5
 		sfx.play_sfx(2)
 		
 	# handle crouch
@@ -73,14 +91,16 @@ func _physics_process(delta):
 	# handle move speed
 	if crouched:
 		speed = CROUCH_SPEED
-	elif Input.is_action_pressed("mov_sprint") and !crouched:
+	elif Input.is_action_pressed("mov_sprint") and player_stats.can_sprint and !crouched and player_stats.stamina > 0:
 		speed = SPRINT_SPEED
 	else:
 		speed = WALK_SPEED
+		
+	if player_stats.stamina <= 0:
+		player_stats.can_sprint = false
+		sprint_cooldown_timer.start()
 
-	# input direction
-	var input_dir = Input.get_vector("mov_left", "mov_right", "mov_forward", "mov_backward")
-	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	# movement
 	if is_on_floor():
 		if direction:
 			velocity.x = direction.x * speed
@@ -99,9 +119,15 @@ func _physics_process(delta):
 	# sfx
 	if direction:
 		if cos(t_bob * BOB_FREQ / 2) > 0.9 and !sfx.sfx_footstep_1.playing:
-			sfx.play_sfx(0)
+			if speed == SPRINT_SPEED:
+				sfx.play_sfx(0, "sprint")
+			else:
+				sfx.play_sfx(0)
 		if cos(t_bob * BOB_FREQ / 2) < -0.9 and !sfx.sfx_footstep_2.playing:
-			sfx.play_sfx(1)
+			if speed == SPRINT_SPEED:
+				sfx.play_sfx(1, "sprint")
+			else:
+				sfx.play_sfx(1)
 
 	# fov
 	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
@@ -122,3 +148,6 @@ func _headbob(time: float) -> Vector3:
 	
 func _on_coyote_time_timeout():
 	grounded = false
+
+func _on_sprint_cooldown_timeout():
+	player_stats.can_sprint = true
--- scripts/player_sfx.gd
+++ scripts/player_sfx.gd
@@ -3,6 +3,8 @@ extends Node3D
 
 @export var footsteps_forest_1: Array = []
 @export var footsteps_forest_2: Array = []
+@export var footsteps_sprint_forest_1: Array = []
+@export var footsteps_sprint_forest_2: Array = []
 @export var jump_forest: Array = []
 @export var land_forest: Array = []
 
@@ -11,12 +13,20 @@ extends Node3D
 @onready var sfx_jump: AudioStreamPlayer3D = get_node("jump")
 @onready var sfx_land: AudioStreamPlayer3D = get_node("land")
 
-func play_sfx(idx: int):
+func play_sfx(idx: int, arg: String = "N/A"):
 	randomize()
 	sfx_footstep_1.pitch_scale = randf_range(0.9, 1.1)
-	sfx_footstep_1.stream = footsteps_forest_1.pick_random()
 	sfx_footstep_2.pitch_scale = randf_range(0.9, 1.1)
-	sfx_footstep_2.stream = footsteps_forest_2.pick_random()
+	if arg == "sprint":
+		sfx_footstep_1.stream = footsteps_forest_1.pick_random()
+		sfx_footstep_1.volume_db = 0
+		sfx_footstep_2.stream = footsteps_forest_2.pick_random()
+		sfx_footstep_2.volume_db = 0
+	else:
+		sfx_footstep_1.stream = footsteps_sprint_forest_1.pick_random()
+		sfx_footstep_1.volume_db = -12
+		sfx_footstep_2.stream = footsteps_sprint_forest_2.pick_random()
+		sfx_footstep_1.volume_db = -12
 	sfx_jump.pitch_scale = randf_range(0.9, 1.1)
 	sfx_jump.stream = jump_forest.pick_random()
 	sfx_land.pitch_scale = randf_range(0.9, 1.1)
